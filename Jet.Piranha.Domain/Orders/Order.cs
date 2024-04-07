3dc9512ea4880e2eed578caf8e119f30f03ecf89
Izaiah Steenrod <steenrod.13@buckeyemail.osu.edu>
2024-04-07 15:35:29 -0400

    finished, untested

---
+++ Jet.Piranha.Domain/Orders/Order.cs
@@ -0,0 +1,23 @@
+using Jet.Piranha.Domain.Catalog;
+
+namespace Jet.Piranha.Domain.Orders
+{
+    public class Orders
+    {
+        public int Id { get; set; }
+        // Initialize the Items list to ensure it's not null.
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public DateTime CreatedDate { get; set; }
+        public decimal TotalPrice => Items.Sum(i => i.Price);
+    }
+
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        // Making Item nullable, see below :)
+        public Item? Item { get; set; }
+        public int Quantity { get; set; }
+        // If Item is null, the Price is considered $0
+        public decimal Price => (Item?.Price ?? 0) * Quantity;
+    }
+}
